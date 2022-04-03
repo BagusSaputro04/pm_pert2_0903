a0a53a68e3d50b5e5694d71dd6baadee7d4a6586
BagusSaputro04 <ajegbimbim3@gmail.com>
2022-04-03 22:59:37 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      title: 'flutter Basic Widget',
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("halamanku"),
+      ),
+      body: const Center(
+        child: Text("hai gays"),
+      ),
+    );
+  }
+}
